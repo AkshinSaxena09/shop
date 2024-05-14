c3a10061b5d5de4b6958602e6a83795e62d639e6
AkshinSaxena09 <saxenaakshin2004@gmail.com>
2024-05-14 23:41:33 +0530

    commit

--- seed.js
+++ seed.js
@@ -6,31 +6,31 @@ const products = [
         name:"iphone 14pro",
         img:'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGFwcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
         price: 140000,
-        desc: "bohat mahenga"
+        desc: "good phone"
     },
     {
         name:"macbook m2",
         img:'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGFwcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
         price: 250000,
-        desc: "aukaat ke bahar"
+        desc: "good laptop"
     },
     {
         name:"iwatch",
         img:'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGFwcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
         price: 70000,
-        desc: "useless product"
+        desc: "good watch"
     },
     {
         name:"ipad",
         img:'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGFwcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
         price: 88000,
-        desc: "badiya cheez"
+        desc: "good thing"
     },
     {
         name:"airpods",
         img:'https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGFwcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
         price: 27000,
-        desc: "vahiyaad thuuu radddi"
+        desc: "good earphones"
     }
 ]
 
